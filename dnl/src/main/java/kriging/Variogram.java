47c7e6b7b85325beb768e3b64e5cdfc6cf2a4464
Ashraf <Ashraf@DESKTOP-MKV5T2O>
2019-05-21 06:33:37 +0800

    Finzalized the variogam Still not tested

--- dnl/src/main/java/kriging/Variogram.java
+++ dnl/src/main/java/kriging/Variogram.java
@@ -1,6 +1,7 @@
 package kriging;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
@@ -8,18 +9,32 @@ import org.apache.commons.math3.linear.OpenMapRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.matsim.core.utils.collections.Tuple;
+import org.nd4j.linalg.checkutil.CheckUtil;
 import org.nd4j.linalg.factory.Nd4j;
 
 import linktolinkBPR.LinkToLinks;
 
 /**
- * This class will calculate the distance between two input of dimension nxt where n is the number of link2link and t is the time slice
+ * This class will calculate the required covariance marices 
  * @author Ashraf
  *
  */
 public class Variogram {
-	private LinkToLinks l2ls;
-	private RealMatrix sigmaMatrix;
+	private final Map<Integer,Tuple<RealMatrix,RealMatrix>> trainingDataSet;
+	private final LinkToLinks l2ls;
+	private final RealMatrix sigmaMatrix;
+	private RealMatrix beta;
+	private Map<String,RealMatrix> varianceMapAll;
+	
+	public Variogram(Map<Integer,Tuple<RealMatrix,RealMatrix>>trainingDataSet,LinkToLinks l2ls) {
+		this.trainingDataSet=trainingDataSet;
+		this.l2ls=l2ls;
+		this.sigmaMatrix=this.calcSigmaMatrix(trainingDataSet);
+		//Initialize beta to a nxt matrix of one
+		this.beta=CheckUtil.convertToApacheMatrix((Nd4j.zeros(trainingDataSet.get(0).getFirst().getRowDimension(),trainingDataSet.get(0).getFirst().getColumnDimension()).addi(1)));
+		this.varianceMapAll=this.calculateVarianceMatrixAll(this.beta);
+	}
+	
 	/**
 	 * 
 	 * @param a first tensor
@@ -32,8 +47,8 @@ public class Variogram {
 	 * @param beta parameters for the variogram
 	 * @return
 	 */
-	public void calcSigmaMatrix(Map<Integer,Tuple<RealMatrix,RealMatrix>>trainingDataSet,double[][]beta) {
-		RealMatrix sd=new Array2DRowRealMatrix();
+	public RealMatrix calcSigmaMatrix(Map<Integer,Tuple<RealMatrix,RealMatrix>>trainingDataSet) {
+		RealMatrix sd=new Array2DRowRealMatrix(trainingDataSet.get(0).getFirst().getRowDimension(),trainingDataSet.get(0).getFirst().getColumnDimension());
 		StandardDeviation sdCalculator= new StandardDeviation();
 		IntStream.rangeClosed(0,trainingDataSet.get(0).getSecond().getRowDimension()-1).forEach((n)->
 		{
@@ -45,7 +60,7 @@ public class Variogram {
 				sd.setEntry(n, t, Math.pow(sdCalculator.evaluate(data),2));
 			});
 		});
-		this.sigmaMatrix=sd;
+		return sd;
 	}
 	
 	public double calcDistance(RealMatrix a,RealMatrix b,LinkToLinks l2ls,int n,int t) {
@@ -60,13 +75,12 @@ public class Variogram {
 	}
 	/**
 	 * This function will calculate the K matrix for a (n,t) pair the dimension of the matrix is IxI where I is the number of data point.
-	 * THe auto co-variance function is sigma^2exp(-1*distance)
+	 * THe auto co-variance function is sigma^2exp(-1*distance*beta)
 	 * @param n the l2l id 
-	 * @param t time id 
-	 * @param trainingDataSet a map containing the training data in id -> (x,y) mapping where x and y are in RealMatrix format 
+	 * @param t time id  
 	 * @return K dim IxI
 	 */
-	public RealMatrix calcVarianceMatrix(int n, int t,Map<Integer,Tuple<RealMatrix,RealMatrix>>trainingDataSet,double[][]beta){
+	public RealMatrix calcVarianceMatrix(int n, int t,RealMatrix beta){
 		double sigma=sigmaMatrix.getEntry(n, t);
 		RealMatrix K=new OpenMapRealMatrix(trainingDataSet.size(),trainingDataSet.size());
 		int i=0;
@@ -76,7 +90,7 @@ public class Variogram {
 				if(i==j) {
 					K.setEntry(i, i, sigma);
 				}else {
-					double v=sigma*Math.exp(-1*this.calcDistance(dataPair1.getFirst(), dataPair2.getFirst(), this.l2ls, n, t)*beta[n][t]);
+					double v=sigma*Math.exp(-1*this.calcDistance(dataPair1.getFirst(), dataPair2.getFirst(), this.l2ls, n, t)*beta.getEntry(n, t));
 					K.setEntry(i, j, v);
 					K.setEntry(j, i, v);
 				}
@@ -91,19 +105,58 @@ public class Variogram {
 	 * @param n
 	 * @param t
 	 * @param X
-	 * @param trainingDataSet
 	 * @param beta
 	 * @return the covariance vector with dimension Ix1
 	 */
-	public RealMatrix calcVarianceVector(int n, int t,RealMatrix X, Map<Integer,Tuple<RealMatrix,RealMatrix>>trainingDataSet,double[][]beta){
+	public RealMatrix calcVarianceVector(int n, int t,RealMatrix X, RealMatrix beta){
 		double sigma=sigmaMatrix.getEntry(n, t);
-		RealMatrix K=new OpenMapRealMatrix(trainingDataSet.size(),1);
+		RealMatrix K=new Array2DRowRealMatrix(trainingDataSet.size(),1);
 		int i=0;
 		for(Tuple<RealMatrix,RealMatrix> dataPair:trainingDataSet.values()) {
-			double v=sigma*Math.exp(-1*this.calcDistance(X, dataPair.getFirst(), this.l2ls, n, t)*beta[n][t]);
+			double v=sigma*Math.exp(-1*this.calcDistance(X, dataPair.getFirst(), this.l2ls, n, t)*beta.getEntry(n, t));
 			K.setEntry(i, 1, v);
 			i++;
 		}
 		return K;
+	}
+	
+	/**
+	 * calculate IxI covariance matrix for all n x t outputs
+	 * @param beta
+	 * @return the map with n_t -> IxI realMatrix covariance matrix
+	 */
+	public Map<String,RealMatrix>calculateVarianceMatrixAll(RealMatrix beta){
+		Map<String,RealMatrix> varianceMatrixAll=new ConcurrentHashMap<>();
+		IntStream.rangeClosed(0,trainingDataSet.get(0).getSecond().getRowDimension()-1).forEach((n)->
+		{
+			IntStream.rangeClosed(0,trainingDataSet.get(0).getSecond().getColumnDimension()-1).forEach((t)->{
+				varianceMatrixAll.put(Integer.toString(n)+"_"+Integer.toString(t),this.calcVarianceMatrix(n, t, beta));
+			});
+		});
+		return varianceMatrixAll;
+	}
+	
+	/**
+	 * calculate Ix1 covariance vector for all n x t outputs
+	 * @param beta
+	 * @return the map with n_t -> Ix1 realMatrix covariance matrix
+	 */
+	public Map<String,RealMatrix>calculateVarianceVectorAll(RealMatrix X,RealMatrix beta){
+		Map<String,RealMatrix> varianceVectorAll=new ConcurrentHashMap<>();
+		IntStream.rangeClosed(0,trainingDataSet.get(0).getSecond().getRowDimension()-1).forEach((n)->
+		{
+			IntStream.rangeClosed(0,trainingDataSet.get(0).getSecond().getColumnDimension()-1).forEach((t)->{
+				varianceVectorAll.put(Integer.toString(n)+"_"+Integer.toString(t),this.calcVarianceVector(n, t, X, beta));
+			});
+		});
+		return varianceVectorAll;
+	}
+	/**
+	 * Update beta and recalculate the IxI variance matrix
+	 * @param beta
+	 */
+	public void updateBeta(RealMatrix beta) {
+		this.beta=beta;
+		this.varianceMapAll=this.calculateVarianceMatrixAll(this.beta);
 	}
 }
