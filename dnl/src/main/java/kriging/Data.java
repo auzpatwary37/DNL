90e322b218048061559ec061a2504fd42531bcba
h <auzpatwary@connect.ust.hk>
2019-07-04 20:06:37 +0800

    added Data

---
+++ dnl/src/main/java/kriging/Data.java
@@ -0,0 +1,33 @@
+package kriging;
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+/**
+ * Class to hold one data instance so that it can be recovered later. 
+ * @author h
+ *
+ */
+public class Data {
+	private final INDArray X;
+	private final INDArray Y;
+	private final String key;
+	
+	public Data(INDArray X,INDArray Y,String key) {
+		this.X=X;
+		this.Y=Y;
+		this.key=key;
+	}
+
+	public INDArray getX() {
+		return X;
+	}
+
+	public INDArray getY() {
+		return Y;
+	}
+
+	public String getKey() {
+		return key;
+	}
+	
+}
