7640bab98dc92485ef1bd5a3b290ec844b37e486
Ashraf <auzpatwary@connect.ust.hk>
2020-06-18 11:50:27 +0800

    Minor change, to make compatible with linux as there is no matlab in linux.

--- dnl/src/main/java/kriging/MatlabInverse.java
+++ dnl/src/main/java/kriging/MatlabInverse.java
@@ -6,11 +6,11 @@ import java.util.List;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
-import com.mathworks.toolbox.javabuilder.MWClassID;
-import com.mathworks.toolbox.javabuilder.MWJavaObjectRef;
-import com.mathworks.toolbox.javabuilder.MWNumericArray;
-
-import CalcInverse.Inverse;
+//import com.mathworks.toolbox.javabuilder.MWClassID;
+//import com.mathworks.toolbox.javabuilder.MWJavaObjectRef;
+//import com.mathworks.toolbox.javabuilder.MWNumericArray;
+//
+//import CalcInverse.Inverse;
 
 
 public class MatlabInverse {
@@ -20,32 +20,33 @@ public class MatlabInverse {
  */
 	public INDArray getInverse(INDArray mat) {
 		
-		Inverse inv = null;
-		MWNumericArray x0 = null;	
-		MWNumericArray x1 = null;	
-		MWJavaObjectRef origRef = null;
-		Object[] result = null;	
-		try
-		{
-
-			/* Instantiate a new Java object */
-			/* This should only be done once per application instance */
-			inv = new Inverse();
-			origRef=new MWJavaObjectRef(this);
-			try {
-				x0 = new MWNumericArray(mat.toDoubleMatrix(), MWClassID.DOUBLE);
-				result=inv.CalcInverse(1,origRef,x0);
-				x1=(MWNumericArray)result[0];
-			}catch(Exception e) {
-				System.out.println(e);
-			}
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-		INDArray out=Nd4j.create((double[][])x1.toDoubleArray());
-		x0.dispose();
-		x1.dispose();
-		inv.dispose();
+//		Inverse inv = null;
+//		MWNumericArray x0 = null;	
+//		MWNumericArray x1 = null;	
+//		MWJavaObjectRef origRef = null;
+//		Object[] result = null;	
+//		try
+//		{
+//
+//			/* Instantiate a new Java object */
+//			/* This should only be done once per application instance */
+//			inv = new Inverse();
+//			origRef=new MWJavaObjectRef(this);
+//			try {
+//				x0 = new MWNumericArray(mat.toDoubleMatrix(), MWClassID.DOUBLE);
+//				result=inv.CalcInverse(1,origRef,x0);
+//				x1=(MWNumericArray)result[0];
+//			}catch(Exception e) {
+//				System.out.println(e);
+//			}
+//		}catch(Exception e) {
+//			System.out.println(e);
+//		}
+//		INDArray out=Nd4j.create((double[][])x1.toDoubleArray());
+		INDArray out=Nd4j.zeros(mat.shape());
+//		x0.dispose();
+//		x1.dispose();
+//		inv.dispose();
 		return out;
 	}
 	
