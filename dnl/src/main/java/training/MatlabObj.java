0adaa2aa990692b7d7faa1c5bb6103c0770a5026
Ashraf Uz Zaman Patwary <auzpatwary@connect.ust.hk>
2020-06-18 11:37:46 +0800

    Important code from old workspace

---
+++ dnl/src/main/java/training/MatlabObj.java
@@ -0,0 +1,13 @@
+package training;
+
+import java.util.LinkedHashMap;
+
+public interface MatlabObj {
+
+	public double evaluateFunction(double[] x);
+	
+	public double evaluateConstrain(double[] x);
+
+	public LinkedHashMap<String, Double> ScaleUp(double[] x);
+	
+}
